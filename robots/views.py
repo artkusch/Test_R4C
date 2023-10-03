4c05641a3b8d73d072711522c74dfeab22950227
Artem <artkushch45@gmail.com>
2023-10-03 17:02:59 +0300

    Xslx export

--- robots/views.py
+++ robots/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
+import openpyxl
+import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
 
 from .models import Robot
 
@@ -25,3 +27,32 @@ def create_robot(request):
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
     else:
         return JsonResponse({'error': 'Invalid HTTP method'}, status=405)
+
+
+
+def export_data_to_xlsx(request):
+    workbook = openpyxl.Workbook()
+
+    versions = Robot.objects.values_list('version', flat=True).distinct()
+
+    models = Robot.objects.values_list('model', flat=True).distinct()
+
+    for model in models:
+        model_sheet = workbook.create_sheet(f'Model {model}')
+
+        headers = ['Модель', 'Версия', 'Количество за неделю']
+        model_sheet.append(headers)
+
+        for version in versions:
+            robot_data = Robot.objects.filter(model=model, version=version)
+            for data_row in robot_data:
+                row = [data_row.model, data_row.version, data_row.count_week]
+                model_sheet.append(row)
+
+    del workbook["Sheet"]
+
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="exported_data.xlsx"'
+    workbook.save(response)
+
+    return response
