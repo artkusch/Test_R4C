b69c152bafac480f5d94f54d0d0f4523e0d74794
Artem <artkushch45@gmail.com>
2023-10-03 16:58:28 +0300

    Add_robot

--- R4C/urls.py
+++ R4C/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from robots.views import create_robot
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create', create_robot),
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+from .models import Robot
+
+
+@csrf_exempt
+def create_robot(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+
+            model = data.get('model')
+            version = data.get('version')
+            created = data.get('created')
+
+            serial = f"{model}-{version}"
+
+            robot = Robot.objects.create(serial=serial, model=model, version=version, created=created)
+
+            response_data = {'message': 'Robot created successfully', 'robot_id': robot.id}
+            return JsonResponse(response_data, status=201)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid HTTP method'}, status=405)
